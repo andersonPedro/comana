9f15f413af705d8dfde6a31671fd0dc0c54adbc3
Anderson Velasco <anderson.pedro192@gmail.com>
2023-06-24 12:53:20 -0300

    feat:pagina sua evoluçao

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats # import your app modules here
+from apps import home, data_stats, sua_evolucao # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Sua evolução", data_stats.app)
+# app.add_app("Data Stats", data_stats.app)
+app.add_app("Sua evolução", sua_evolucao.app)
 
 # The main app
 app.run()
--- apps/sua_evolucao.py
+++ apps/sua_evolucao.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from data.create_data import create_table
+from io import StringIO
+
 
 def app():
     st.title('Sua evolução')
 
-    st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
+    uploaded_file = st.file_uploader(label="Colete o arquivo do seu portal do aluno em formato json e suba aqui.")
 
-    st.markdown("### Plot Data")
-    df = create_table()
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
 
-    st.line_chart(df)
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
