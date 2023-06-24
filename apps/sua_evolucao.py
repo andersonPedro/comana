377462653ac52bc760121229576ac720bfb2b125
Anderson Velasco <anderson.pedro192@gmail.com>
2023-06-24 13:04:41 -0300

    feat: analises primarias evolucao

--- apps/sua_evolucao.py
+++ apps/sua_evolucao.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from io import StringIO
+from data.create_data import create_df_grade_horas, create_df_bacharelado_ciencia_tecnologia
 
 
 def app():
@@ -10,18 +11,22 @@ def app():
     uploaded_file = st.file_uploader(label="Colete o arquivo do seu portal do aluno em formato json e suba aqui.")
 
     if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+        # Can be used wherevern a "file-like" object is accepted:
+        df_ficha = pd.read_json(uploaded_file)
+        st.write("Matérias já realizadas:")
+        st.write(df_ficha)
 
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+        df_bacharelado_ciencia_tecnologia = create_df_bacharelado_ciencia_tecnologia()
+        df_grade_horas = create_df_grade_horas()
 
-        # To read file as string:
-        string_data = stringio.read()
-        st.write(string_data)
+        df_materias_faltantes = df_bacharelado_ciencia_tecnologia[~df_bacharelado_ciencia_tecnologia['codigo'].isin(df_ficha['codigo'])]
 
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+        ficha_soma_creditos_total = df_ficha[df_ficha["situacao"] == "Aprovado"]['creditos'].sum()
+        grade_soma_creditos_total = df_grade_horas[df_grade_horas["categoria"] == "Carga horária total do curso"]["creditos"].sum()
+        diferenca = grade_soma_creditos_total - ficha_soma_creditos_total
+        percentual_progresso = round((ficha_soma_creditos_total / grade_soma_creditos_total) * 100, 2)
+
+        st.write("Total de Créditos já realizados:", ficha_soma_creditos_total)
+        st.write("Total de Créditos do curso:" ,grade_soma_creditos_total)
+        st.write("Progresso:", percentual_progresso)
+        st.write("Quantos créditos faltam:", diferenca)
