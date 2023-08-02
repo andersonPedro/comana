853ae1d66d956d468415ddc3fe8f43d231c19683
Anderson Velasco <anderson.pedro192@gmail.com>
2023-08-01 22:58:41 -0300

    versao beta

--- apps/home.py
+++ apps/home.py
@@ -1,25 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from data.create_data import create_df_grade_horas, create_df_bacharelado_ciencia_tecnologia
+from data.create_data import create_livro, create_aprendendo
 
 def app():
-    st.write("## Projeto Comana")
-    st.write("Veja o quanto ainda falta para você se formar")
-   
+    st.title("Projeto Comana")
+    # st.image(create_livro())
+    st.image(create_aprendendo())
     st.divider()
-    st.write("##### Grade de Horas do Bacharelado em Ciencia e Tecnologia")
-    df_grade_horas = create_df_grade_horas()
-    st.write(df_grade_horas)
-    st.divider()
-
-    st.write("##### Matérias do Bacharelado em Ciencia e Tecnologia")
-    df_bacharelado_ciencia_tecnologia = create_df_bacharelado_ciencia_tecnologia()
-    st.write(df_bacharelado_ciencia_tecnologia)
-    st.divider()
-
-
-
-    st.write('Engenharia Unificada')
-
-
+    st.write("### Por estudantes da UFABC para estudantes da UFABC")
+    st.write("Este é um projeto realizado na disciplina Engenharia Unificada I com o objetivo de aproveitar a oportunidade de criar uma visão estratégica sobre a grade curricular do aluno")
+    st.write("Na aba Evolução, suba seu arquivo JSON disponibilizado no portal do aluno. Com isso, a plataforma conseguirá lhe retornar uma análise precisa sobre sua grade, assim como realizar sugestão de próximas disciplinas de acordo com sua grade de pós-BI.")
