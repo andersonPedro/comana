389a05435a6fdb67baf7ff4440a8d357978cbed8
Andevel <anderson.pedro192@gmail.com>
2023-12-03 13:04:20 -0300

    primeira_versao

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, sua_evolucao, plano_pedagogico
+from apps import home, sua_evolucao
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Sua evolução", sua_evolucao.app)
-app.add_app("Planos Pedagógicos", plano_pedagogico.app)
 
 # The main app
 app.run()
